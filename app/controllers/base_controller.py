2e80cb1213f37a8727629893a9979d256b4592be
Tifano Eng Ogotan <107922211+TifanoOgotan@users.noreply.github.com>
2025-08-28 17:13:57 +0700

    Update base_controller.py

--- app/controllers/base_controller.py
+++ app/controllers/base_controller.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+import os
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash, send_from_directory, current_app
 from app.daos import menu_dao, user_dao, transaksi_dao
-from datetime import date, timedelta
+from datetime import date
 
 base_bp = Blueprint('base', __name__, url_prefix='/')
 
@@ -9,29 +10,43 @@ base_bp = Blueprint('base', __name__, url_prefix='/')
 def home():
     if not session.get('person'):
         flash("Anda Harus Login !!!", "unauthorized")
-        return redirect(url_for('auth.login'))  # Arahkan ke endpoint login
+        return redirect(url_for('auth.login'))
+
     transaksi = transaksi_dao.get_transaksi_by_date(date.today(), date.today())
     total_transaksi = len(transaksi)
     pendapatan = 0
     total_bb = 0
-    if transaksi :
-        for trx in transaksi :
+
+    if transaksi:
+        for trx in transaksi:
             if trx['status_bayar'] == 'BB':
                 total_bb += 1
-            else :
+            else:
                 pendapatan += trx['total_harga']
+
     data = {
         "total_transaksi": total_transaksi,
         "pendapatan": f"{pendapatan:,}".replace(",", "."),
         "total_bb": total_bb
     }
 
-    return render_template('dashboard.html',data=data)
+    return render_template('dashboard.html', data=data)
 
+
+# Endpoint manifest.json
 @base_bp.route('/manifest.json')
 def manifest():
     return send_from_directory(
-        os.path.join(os.path.dirname(__file__), 'static'),
+        os.path.join(current_app.root_path, 'static'),
         'manifest.json',
         mimetype='application/manifest+json'
+    )
+
+# Endpoint service-worker.js
+@base_bp.route('/service-worker.js')
+def service_worker():
+    return send_from_directory(
+        os.path.join(current_app.root_path, 'static'),
+        'service-worker.js',
+        mimetype='application/javascript'
     )
